9f08f7c5941f19325839147894d8f83a3ea23bb3
Jacob McKenzie <mandrake@Jacobs-MacBook-Pro.local>
2014-05-22 14:08:34 -0400

    Update
    Corrected comments and simplified output argument handling

--- sal_fuelinj.m
+++ sal_fuelinj.m
@@ -1,11 +1,11 @@
-function [m_fuel, varargout] = sal_fuelinj(p_fuel, pw_fuel, varargin)
+function [m_fuel, LHV, e_evap] = sal_fuelinj(p_fuel, pw_fuel, varargin)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                                         %
 %  sal_fuelinj - compute the mass of fuel injected in one injection event %
 %                                                                         %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% sal_fuelinj -  version 0.91 - Jacob E. McKenzie - mofified: 04/29/14
+% sal_fuelinj -  version 0.92 - Jacob E. McKenzie - mofified: 05/05/14
 % 
 % inputs:
 %  - p_fuel	 [Mpa] : fuel rail pressure
@@ -13,8 +13,9 @@ function [m_fuel, varargout] = sal_fuelinj(p_fuel, pw_fuel, varargin)
 %  - fuel	 [str] : fuel name, string input
 %
 % output:
-%  - m_fuel	  [g]    : mass of fuel injected in one injection event
+%  - m_fuel	  [kg]   : mass of fuel injected in one injection event
 %  - LHV_fuel [J/kg] : lower heating value of fuel
+%  - e_evap   [J]    : energy consumed by fuel evaporation
 %
 % notes:
 %  current version does not include any error checking. p_fuel and pw_fuel
@@ -25,9 +26,10 @@ function [m_fuel, varargout] = sal_fuelinj(p_fuel, pw_fuel, varargin)
 
 % parse optional arguments
 narginchk(2,3);
-nargoutchk(1,2);
 if (nargin == 3)
   fuel = varargin{1};
+else
+  fuel = 'HF0437';
 end
 
 
@@ -36,13 +38,15 @@ switch upper(fuel)
     % Haltermann HF0437 96 RON ref. fuel
     AFR = 14.6742;
     rho = 0.7374;
-    LHV = 44.0e6;
+    LHV = 44.0e6; % [J/kg]
+    h_v = 350e3;  % [J/kg]
   
   otherwise
-    % case for unidentifiable fuel type (assume gasoline)
+    % case for unidentifiable fuel type (assume generic gasoline)
     AFR = 14.7;
     rho = 0.744;
-    LHV = 44.0e6;
+    LHV = 44.0e6; % [J/kg]
+    h_v = 350e3;  % [J/kg]
 end
 
 % calibration  map of fuel injector. At the pressures given by p_ref the
@@ -51,18 +55,13 @@ end
 % and the flow rate is measured again. t_ref is the delay during which no
 % fuel is injected. 
 p_ref = [4.081513524; 7.095787287; 10.0785737; 15.0989902]; % [MPa]
-v_ref = [10.34; 13.61; 16.40; 20.20];			    % [mg/ms]
-t_ref = [0.0517; 0.0753; 0.0848; 0.1039];		    % [ms]
+v_ref = [10.34; 13.61; 16.40; 20.20];			            % [mg/ms]
+t_ref = [0.0517; 0.0753; 0.0848; 0.1039];		            % [ms]
 rho_ref = 0.744; % density of calibration fuel
 
 slope  = interp1(p_ref,v_ref,p_fuel);
 offset = interp1(p_ref,t_ref,p_fuel);
 
-m_fuel = ((pw_fuel-offset).*slope*(rho/rho_ref))*10^(-6); % fuel in kg
-
-% if LHV is requested, return it.
-if (nargout == 2)
-    varargout{1} = LHV;
-end
-
+m_fuel = ((pw_fuel-offset).*slope*(rho/rho_ref))*10^(-6); % [kg]
+e_evap = h_v*m_fuel;                                      % [J]
 end
